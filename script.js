8ab72810731be8039db1000af5e3122d65caf208
Maria Amaba <142577342+marimo44@users.noreply.github.com>
2024-07-17 17:41:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ dateOptionsSelectElement.addEventListener("change", () => {
       currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
       break;
     default:
-      currentDateParagraph.textContent = formattedDate;    
+    currentDateParagraph.textContent = formattedDate;
+    
   }
 });
